f9f71b581af40ce47fe7eb5a7bf592d23c0ca96a
AlejandraGomez26 <alejagomez.026@gmail.com>
2023-04-13 20:43:29 -0500

    while

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// el i++ no se coloca adentro al igual que el break  se usa para salir de un ciclo 
+// el return de toda la funcion
+// el while es como un for
+let i =0
+while ( i <= 10){
+    console.log (i)
+    i++
+    
+}
